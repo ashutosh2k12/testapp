aabf17f84291e4bb935e357f29eba0740aa56c47
ashutosh2k12 <216.ashutosh@gmail.com>
2014-05-16 20:52:00 +0530

    bt

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,33 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+         $(document).on("click", ".uib_w_5", function(evt)
+        {
+         activate_page("#uib_page_1"); 
+        });
+        $(document).on("click", ".uib_w_18", function(evt)
+        {
+         activate_subpage("#uib_page_2sub"); 
+        });
+        $(document).on("click", ".uib_w_10", function(evt)
+        {
+         activate_page("#uib_page_2"); 
+        });
+        $(document).on("click", ".uib_w_14", function(evt)
+        {
+         activate_subpage("#uib_page_3"); 
+        });
+        $(document).on("click", ".uib_w_16", function(evt)
+        {
+         activate_subpage("#uib_page_3"); 
+        });
+}
+ $(document).ready(register_event_handlers);
+})();
