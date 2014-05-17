25d60f52a9d54b94f7746891e6397ec92ad0ef62
ashutosh2k12 <216.ashutosh@gmail.com>
2014-05-17 17:11:31 +0530

    app fm

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -13,7 +13,6 @@
         {
          activate_subpage("#uib_page_3"); 
         });
-        
         $(document).on("click", ".uib_w_15", function(evt)
         {
          activate_subpage("#uib_page_3"); 
@@ -21,18 +20,6 @@
         $(document).on("click", ".uib_w_16", function(evt)
         {
          activate_subpage("#uib_page_3"); 
-        });
-        $(document).on("click", ".uib_w_10", function(evt)
-        {
-         activate_page("#uib_page_2"); 
-        });
-        $(document).on("click", ".uib_w_5", function(evt)
-        {
-         activate_page("#uib_page_1"); 
-        });
-        $(document).on("click", ".uib_w_18", function(evt)
-        {
-         activate_subpage("#uib_page_2sub"); 
         });
 }
  $(document).ready(register_event_handlers);
