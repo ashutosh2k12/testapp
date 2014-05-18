c352b740ab58f41494d340737c05380a5d6d5ac9
ashutosh2k12 <216.ashutosh@gmail.com>
2014-05-19 01:36:51 +0530

    slidemenu

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -135,7 +135,7 @@ function register_event_handlers()
 		$(document).on("click", ".uib_w_27", function(evt)
         {
 		//	uib_sb.toggle_sidebar($(".uib_w_22"));  	
-			alert('Hello world');
+		//	alert('Hello world');
 			$.ui.toggleSideMenu();
         });
 
