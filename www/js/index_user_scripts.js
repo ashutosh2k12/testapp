cdce87dc6e977b1bb8cbcc5ab8f935a9bb30c7ad
ashutosh2k12 <216.ashutosh@gmail.com>
2014-05-20 02:51:13 +0530

    js

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -38,7 +38,8 @@ function showPushes(div)
 			var _cont = '';
 			for(var i=0;i<pushes.length;i++)
 			{
-			 _cont += '<li class="widget uib_w_list list-push push'+i+'" data-uib="app_framework/listitem" data-ver="0" data-push=\''+pushes[i]+'\'>'+pushes[i].message+'</li>';
+			  var jsdata = { appid : pushes[i].appid, pin: pushes[i].pin };
+			 _cont += '<li class="widget uib_w_list list-push" data-uib="app_framework/listitem" data-ver="0" data-push=\''+jsdata+'\'>'+pushes[i].message+'</li>';
 			}
 			$('ul#pushesfetch').empty().append(_cont);
 		 }
